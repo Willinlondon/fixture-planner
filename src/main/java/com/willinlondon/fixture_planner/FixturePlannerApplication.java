62f9d2c91468086e933d1f3a1bbbfb1ec08c3bfa
William Day <williamjday11@gmail.com>
2026-08-31 12:31:40 +0200

    Initial Spring Boot skeleton via Spring Initializr

---
+++ src/main/java/com/willinlondon/fixture_planner/FixturePlannerApplication.java
@@ -0,0 +1,13 @@
+package com.willinlondon.fixture_planner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FixturePlannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FixturePlannerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/willinlondon/fixture_planner/FixturePlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.willinlondon.fixture_planner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FixturePlannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
